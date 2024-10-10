94bb5f90c97e02055bcba40b4cf00214bd16cc39
Koen <koen@venly.io>
2024-10-10 13:33:22 +0200

    refactor(docker): streamline Dockerfile and remove unnecessary echo statements in main.tsx

--- connect-sdk/src/main.tsx
+++ connect-sdk/src/main.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './css/index.css';
-import 'react-loading-skeleton/dist/skeleton.css';
-import App from './App.tsx';
-import Login from './pages/Login.tsx';
-import Home from './pages/Home.tsx';
-import Wallet from './pages/Wallet.tsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './css/index.css'
+import 'react-loading-skeleton/dist/skeleton.css'
+import App from './App.tsx'
+import Login from './pages/Login.tsx'
+import Home from './pages/Home.tsx'
+import Wallet from './pages/Wallet.tsx'
 
-// Logging statements
-console.log('Application is starting...');
-console.log('VITE_CLIENT_ID:', import.meta.env.VITE_CLIENT_ID);
-console.log('VITE_ENV:', import.meta.env.VITE_ENV);
 
 const router = createBrowserRouter([
   {
@@ -29,20 +25,14 @@ const router = createBrowserRouter([
         path: '/wallets/:id',
         element: <Wallet />,
       },
-    ],
+    ]
   },
-]);
+])
 
-// Error handling during application initialization
-try {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <div className="app">
-        <RouterProvider router={router} />
-      </div>
-    </React.StrictMode>
-  );
-  console.log('Application initialized successfully.');
-} catch (error) {
-  console.error('Error during initialization:', error);
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>
+)
