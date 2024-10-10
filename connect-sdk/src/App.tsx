faf279cdd9138b51715ba7b32eecd65bab6b576a
Koen <koen@venly.io>
2024-10-10 14:00:03 +0200

    style(App): fix formatting issues and add console logging for authentication checks

--- connect-sdk/src/App.tsx
+++ connect-sdk/src/App.tsx
@@ -1,54 +1,59 @@
-import { useEffect, useState } from 'react'
-import { Outlet, useLocation, useNavigate } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-import Modal from 'react-modal'
-import { venlyConnect } from './libs/venlyConnect'
-import Alerts from './components/Alerts'
-import Nav from './components/Nav'
-import Loading from './pages/Loading'
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Modal from 'react-modal';
+import { venlyConnect } from './libs/venlyConnect';
+import Alerts from './components/Alerts';
+import Nav from './components/Nav';
+import Loading from './pages/Loading';
 
-Modal.setAppElement('#root')
+Modal.setAppElement('#root');
 
 export default function App() {
   const navigate = useNavigate();
   const location = useLocation();
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    checkAuthenticated()
-  }, [])
+    console.log('Checking authentication...');
+    checkAuthenticated();
+  }, []);
 
   async function checkAuthenticated() {
     try {
-      const res = await venlyConnect.checkAuthenticated()
-      if (!res?.isAuthenticated)
-        navigate('/login', { replace: true })
+      const res = await venlyConnect.checkAuthenticated();
+      console.log('Authentication response:', res);
+      if (!res?.isAuthenticated) {
+        console.log('User not authenticated, redirecting to login...');
+        navigate('/login', { replace: true });
+      }
+    } catch (error) {
+      console.error('Error during authentication:', error);
+      navigate('/login', { replace: true });
     }
-    catch(error) {
-      console.log('error')
-      navigate('/login', { replace: true })
-    }
-    setIsLoading(false)
+    setIsLoading(false);
   }
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
+  } else if (location.pathname === '/login') {
+    return <Outlet />;
   }
-  else if (location.pathname == '/login')
-    return <Outlet />
 
-  return <>
-    <Nav />
-    <div className="main">
-      <Outlet />
-    </div>
-    <Alerts />
-    <Toaster
-      position="bottom-center"
-      containerClassName="toast-container"
-      toastOptions={{
-        className: "toast"
-      }}
-    />
-  </>
+  return (
+    <>
+      <Nav />
+      <div className="main">
+        <Outlet />
+      </div>
+      <Alerts />
+      <Toaster
+        position="bottom-center"
+        containerClassName="toast-container"
+        toastOptions={{
+          className: 'toast',
+        }}
+      />
+    </>
+  );
 }
